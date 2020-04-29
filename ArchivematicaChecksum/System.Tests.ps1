db55aa0b59b2d2efbad8b90b091579d2cf7a42e3
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-04-28 21:03:41 -0500

    Use TestDrive in system tests for safety

--- ArchivematicaChecksum/System.Tests.ps1
+++ ArchivematicaChecksum/System.Tests.ps1
@@ -1,15 +1,21 @@
-$GLOBAL:TestEnvironmentFolder = "$PSScriptRoot\TestEnvironment"
+$GLOBAL:TestEnvironmentFolder = "TestDrive:\TestEnvironment"
 $GLOBAL:InitialLocation = $Null
 
 Function TestSetup {
     Import-Module $PSScriptRoot\ArchivematicaChecksum.psm1
     $GLOBAL:InitialLocation = Get-Location
+    Set-Location 'TestDrive:\'
+    New-Item -Path $GLOBAL:TestEnvironmentFolder -ItemType Directory
+}
+
+Function ResetState {
+    Set-Location 'TestDrive:\'
+    Remove-Item -Path $GLOBAL:TestEnvironmentFolder -Recurse -Force
     New-Item -Path $GLOBAL:TestEnvironmentFolder -ItemType Directory
 }
 
 Function TestTeardown {
     Set-Location $GLOBAL:InitialLocation
-    Remove-Item -Path $GLOBAL:TestEnvironmentFolder -Recurse -Force
 }
 
 Describe 'System Tests' {
